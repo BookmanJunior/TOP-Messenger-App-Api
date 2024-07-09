66be43337a2fee0c907829630e96984677f6a558
Azam Gulov <gulovazam@gmail.com>
2024-07-09 17:53:24 +0500

    Add user_post middleware
    initial commit

---
+++ src/Controllers/Users.ts
@@ -0,0 +1,65 @@
+import { Request, Response, NextFunction } from "express";
+import { body, validationResult } from "express-validator";
+// import { hashSync, genSalt } from "bcrypt";
+import dbquery from "../DB/DB";
+import { User } from "../Types/User";
+
+export const user_post = [
+  body("name", "Name should be between 2 and 30 characters long.")
+    .trim()
+    .isLength({ min: 2, max: 30 })
+    .escape(),
+  body("password", "Password must be at least 8 characters long.")
+    .trim()
+    .isLength({ min: 8 })
+    .escape(),
+  body(
+    "confirmPassword",
+    "Confirm Password must be at least 8 characters long."
+  )
+    .trim()
+    .isLength({ min: 8 })
+    .custom((confirmPassword, { req }) => {
+      const { password } = req.body as User;
+      if (confirmPassword !== password)
+        throw new Error("Passwords don't match");
+    }),
+  body("username", "Username must be between 2 and 30 characters long")
+    .trim()
+    .isLength({ min: 2, max: 30 })
+    .bail()
+    .escape()
+    .custom(async (value: string) => {
+      const res = await dbquery(
+        "SELECT USERNAME FROM USERS WHERE USERNAME = $1",
+        [value]
+      );
+
+      console.log(res.rows);
+
+      if (res.rows.length)
+        throw new Error("Username exists. Please pick a different username.");
+    }),
+
+  async (
+    req: Request<Record<string, never>, Record<string, never>, User>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const errors = validationResult(req);
+    console.log("Called");
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.mapped());
+    }
+
+    try {
+      const queryString = `INSERT INTO USERS (name, username, password) VALUES($1, $2, $3) RETURNING *`;
+      const queryParams = [req.body.name, req.body.username, req.body.password];
+      const queryRes = await dbquery(queryString, queryParams);
+      res.status(200).send(queryRes);
+    } catch (error) {
+      next(error);
+    }
+  },
+];
