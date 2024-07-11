e816e6e2be69c494a8202a7f418cbf5d6a8925ed
Gulov Azam <gulovazam@gmail.com>
2024-07-12 00:38:36 +0500

    Setup express-session
    initialize passport and passport sesion

--- src/App.ts
+++ src/App.ts
@@ -1,14 +1,36 @@
-import express from "express";
+import express, { CookieOptions } from "express";
 import logger from "morgan";
+import session from "express-session";
+import passport from "passport";
 
 import UserRouter from "./Routes/Users";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const cookieOptions: CookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "DEVELOPMENT" ? false : true,
+  sameSite: process.env.NODE_ENV === "DVELOPMENT" ? "strict" : "none",
+  maxAge: 168 * 60 * 60 * 1000,
+};
+
+if (!process.env.COOKIE_SECRET) {
+  throw Error("Forgot to set cookie's secret");
+}
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: cookieOptions,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/users", UserRouter);
 
