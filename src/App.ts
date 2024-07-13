957582dba244284c4036e4081ebf5b833aa9a807
Gulov Azam <gulovazam@gmail.com>
2024-07-13 16:21:06 +0500

    Serialize user on successful login
    deserializeUser on subsequent requets
    add user to locals on successful login

--- src/App.ts
+++ src/App.ts
@@ -1,16 +1,25 @@
-import express, { CookieOptions } from "express";
+import express, {
+  CookieOptions,
+  NextFunction,
+  Request,
+  Response,
+} from "express";
 import logger from "morgan";
 import session from "express-session";
 import passport from "passport";
+import { LocalStrategy } from "./Auth/LocalStrategy";
+import dbquery from "./DB/DB";
+import { User } from "./Types/User";
 
 import UserRouter from "./Routes/Users";
+import SessionRouter from "./Routes/Session";
 
 const app = express();
 const port = process.env.PORT || 3000;
 const cookieOptions: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "DEVELOPMENT" ? false : true,
-  sameSite: process.env.NODE_ENV === "DVELOPMENT" ? "strict" : "none",
+  sameSite: process.env.NODE_ENV === "DEVELOPMENT" ? "strict" : "none",
   maxAge: 168 * 60 * 60 * 1000,
 };
 
@@ -29,10 +38,32 @@ app.use(
     cookie: cookieOptions,
   })
 );
+passport.use(LocalStrategy);
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id: string, done) => {
+  const queryString = "SELECT USERNAME, NAME FROM USERS WHERE ID = $1";
+  dbquery<User>(queryString, [id])
+    .then((user) => {
+      done(null, user.rows[0]);
+    })
+    .catch((err: unknown) => {
+      done(err, false);
+    });
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.locals.user = req.user;
+  next();
+});
+
 app.use("/users", UserRouter);
+app.use("/session", SessionRouter);
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
