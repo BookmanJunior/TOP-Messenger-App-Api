b7389b06aa9aaff6bb8c436c0c569d3d5acb127f
Gulov Azam <gulovazam@gmail.com>
2024-07-13 16:09:49 +0500

    Add session_get middleware

--- src/Controllers/Session.ts
+++ src/Controllers/Session.ts
@@ -2,6 +2,13 @@ import passport from "passport";
 import { Request, Response, NextFunction } from "express";
 import { User } from "../Types/User";
 
+export const session_get = (req: Request, res: Response) => {
+  if (!res.locals.user) {
+    res.sendStatus(401);
+  }
+  res.status(200).send(res.locals.user);
+};
+
 export const session_post = (
   req: Request,
   res: Response,
