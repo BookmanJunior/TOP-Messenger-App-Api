6db8db3e4d375baabe008cb3e22abe20d2209c1d
Gulov Azam <gulovazam@gmail.com>
2024-07-13 16:18:17 +0500

    Add get route

--- src/Routes/Session.ts
+++ src/Routes/Session.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { session_post } from "../Controllers/Session";
+import { session_get, session_post } from "../Controllers/Session";
 
 const router = Router();
+
+router.get("/", session_get);
 
 router.post("/", session_post);
 
