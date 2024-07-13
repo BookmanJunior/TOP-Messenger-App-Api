66bf555d54dfa36fe98f4b73df520377c6f1a909
Gulov Azam <gulovazam@gmail.com>
2024-07-13 15:56:40 +0500

    Add session_post middleware

--- src/Controllers/Session.ts
+++ src/Controllers/Session.ts
@@ -1,1 +1,26 @@
-export const session_post = () => {};
+import passport from "passport";
+import { Request, Response, NextFunction } from "express";
+import { User } from "../Types/User";
+
+export const session_post = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  passport.authenticate("local", (err: unknown, user: User) => {
+    console.log(user);
+    if (err) {
+      next(err);
+      return;
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.sendStatus(200);
+    });
+  })(req, res, next);
+};
